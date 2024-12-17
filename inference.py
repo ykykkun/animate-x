7f74026ca183151735e47da507bacf3d8ba7ec4c
gongbiao <gongbiao.gb@alibaba-inc.com>
2024-12-17 15:14:28 +0800

    Remove unused imports for better code clarity.

--- inference.py
+++ inference.py
@@ -11,8 +11,6 @@ import numpy as np
 from utils.config import Config
 from utils.registry_class import INFER_ENGINE
 
-from tools import *
-
 if __name__ == '__main__':
     cfg_update = Config(load=True)
     INFER_ENGINE.build(dict(type=cfg_update.TASK_TYPE), cfg_update=cfg_update.cfg_dict)
