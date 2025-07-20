0697f36ae0d99d751e4a662d868be44ea8c525d4
Yue Kun <yuekundev@163.com>
2025-07-20 12:59:33 +0800

    安装

---
+++ install.sh
@@ -0,0 +1,4 @@
+# 安装依赖
+pip install -r requirements.txt --no-cache-dir
+# 下载模型
+huggingface-cli download --token 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 --resume-download Shuaishuai0219/Animate-X --local-dir checkpoints
